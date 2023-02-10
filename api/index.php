57fa0061f651777a91a53ab46a0d5245727886bb
Juscelino Campos <celino3x@gmail.com>
2023-02-10 16:00:42 -0300

    .htaccess e index.php var_dump

--- api/index.php
+++ api/index.php
@@ -1,5 +1,11 @@
 <?php
 
-phpinfo();
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
 
+date_default_timezone_set("America/Sao_Paulo");
+
+$path =explode("/", $_GET['path']);
+
+var_dump($path);
 ?>
