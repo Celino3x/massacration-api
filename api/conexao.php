f9d93564331f332f8aa239d39e0640439d9bbf5b
Juscelino Campos <celino3x@gmail.com>
2023-02-11 13:25:45 -0300

    :sparkles: Inclusão de conexao.php

---
+++ api/conexao.php
@@ -0,0 +1,8 @@
+<?php  
+    $server = "containers-us-west-145.railway.app";
+    $database = "railway";
+    $username = "root";
+    $pass = "Bd1TV2L984VezBqiAKH9";
+
+    $conexao = mysqli_connect($server,$username,$pass,$database);
+?>
