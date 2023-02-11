4630569e71a82f7df319b3e56dd4673134640d1e
Juscelino Campos <celino3x@gmail.com>
2023-02-11 09:37:02 -0300

    cadastrar integrantes e index.php

---
+++ api/cadastrar_integrante.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Massacration | Registration</title>
+</head>
+<body>
+    <div class="page-wrapper bg-red p-t-180 p-b-100 font-robo">
+        <div class="wrapper wraooer--w960">
+            <div class="card card-2">
+                <div class="card-heading"></div>
+                <div class="card-body">
+                    <h2 class="title">Registration</h2>
+                    <form action="" method="POST">
+                        <div class="input-group">
+                            <input placeholder="Nome" type="text" class="input--style-2">
+                        </div>
+                        <div class="row row-space"></div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>
--- api/index.php
+++ api/index.php
@@ -1,11 +1,1 @@
 <?php
-
-header('Access-Control-Allow-Origin: *');
-header('Content-type: application/json');
-
-date_default_timezone_set("America/Sao_Paulo");
-
-$path =explode("/", $_GET['path']);
-
-var_dump($path);
-?>
